e20eaedafe96a10b32e615f035458e3ce50db267
LeticiaGenadeze <lety_genadeze@hotmail.com>
2020-05-17 20:46:08 -0300

    Crud  controller paginas

--- app/Http/Controllers/Admin/PaginasController.php
+++ app/Http/Controllers/Admin/PaginasController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admin\Pagina;
+use App\Models\Admin\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PaginasController extends Controller
 {
@@ -15,17 +18,12 @@ class PaginasController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $pagina = Post::where('tipo', 'pagina')->orderBy('created_at','DESC')->get();
+        if(!$pagina){
+            return response()->json([
+                'error' => 'Nenhuma pagina encontrada!'], 404);
+        }
+        return $pagina;
     }
 
     /**
@@ -36,7 +34,43 @@ class PaginasController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data =  $request->all();
+        $validation = Validator::make($data,[
+            'nome' => 'required|unique:posts,nome',
+            'descricao' => 'required',
+            'capa' => 'required|image|mimes:jpeg,jpg,png,max:2048'
+        ]);
+
+        if($validation->fails()){
+            $errors = $validation->errors();
+            return $errors->toJson();
+        }
+        $data['slug'] = Str::slug($data['nome'], '-');
+
+        if($data['capa'] && $data['capa']!= null){
+            $name = $data['slug'].time();
+            $extension = $request->capa->extension();
+            $nameFile = "{$name}.{$extension}";
+            $upload = $request->capa->storeAs('public/paginas', $nameFile);
+            if ( !$upload ){
+                return response()->json([
+                    'error' => 'Falha ao fazer upload!'], 404);
+            }
+            $data['capa'] = $nameFile;
+        }
+        $data['tipo'] = 'pagina';
+        $pagina = new Post();
+
+        $pagina->fill($data);
+
+        if ($pagina->save()){
+            return response()->json([
+                'success' => 'Página salva com Sucesso!'], 201);
+        }
+
+        return response()->json([
+            'error' => 'Erro ao cadastrar a Página!'], 500);
+
     }
 
     /**
@@ -45,20 +79,10 @@ class PaginasController extends Controller
      * @param  \App\Models\Admin\Pagina  $pagina
      * @return \Illuminate\Http\Response
      */
-    public function show(Pagina $pagina)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Admin\Pagina  $pagina
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Pagina $pagina)
-    {
-        //
+        $pagina = Post::query()->where('id', $id)->get();
+        return $pagina;
     }
 
     /**
@@ -68,9 +92,55 @@ class PaginasController extends Controller
      * @param  \App\Models\Admin\Pagina  $pagina
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Pagina $pagina)
+    public function update(Request $request, Post $pagina)
     {
-        //
+        $data =  $request->all();
+        $validation = Validator::make($data,[
+            'nome' => 'required',
+            'descricao' => 'required'
+        ]);
+
+        if($validation->fails()){
+            $errors = $validation->errors();
+            return $errors->toJson();
+        }
+        $data['slug'] = Str::slug($data['nome'], '-');
+
+        if($data['capa']){
+            $validation = Validator::make($data,[
+               'capa' => 'image|mimes:jpeg,jpg,png,max:2048'
+            ]);
+
+            if($validation->fails()){
+                $errors = $validation->errors();
+                return $errors->toJson();
+            }
+
+            $name = $data['slug'].time();
+            $extension = $request->capa->extension();
+            $nameFile = "{$name}.{$extension}";
+            $upload = $request->capa->storeAs('public/paginas', $nameFile);
+            if ( !$upload ){
+                return response()->json([
+                    'error' => 'Falha ao fazer upload!'], 404);
+            }
+            if($pagina->capa){
+                $capa = public_path('storage/paginas/'.$pagina->capa);
+                if(File::exists($capa)) {
+                    unlink($capa);
+                }
+            }
+            $data['capa'] = $nameFile;
+        }
+
+        $pagina->fill($data);
+        if ($pagina->save()){
+            return response()->json([
+                'success' => 'Pagina atualizada com Sucesso!'], 201);
+        }
+
+        return response()->json([
+            'error' => 'Erro ao atualizar a Pagina!'], 500);
     }
 
     /**
@@ -79,8 +149,22 @@ class PaginasController extends Controller
      * @param  \App\Models\Admin\Pagina  $pagina
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Pagina $pagina)
+    public function destroy($id)
     {
-        //
+        $pagina = Post::where('id', $id)->first();
+        if($pagina){
+            if ($pagina->capa){
+                $capa = public_path('storage/paginas/'.$pagina->capa);
+                if(File::exists($capa)) {
+                    unlink($capa);
+                }
+            }
+           if (!$pagina->delete()){
+            return response()->json([
+                'error' => 'Não foi possivel deletar a Pagina!'], 500);
+            }
+            return response()->json([
+                'success' => 'Pagina deletada com Sucesso!'], 200);
+        }
     }
 }
